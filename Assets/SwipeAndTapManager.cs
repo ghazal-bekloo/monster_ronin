94b2480d51bb0825c7bd50ac8db3ff963e8e99a1
ghazal-bekloo <ghazal.beklo@gmail.com>
2026-01-29 10:14:54 +0330

    monsterronin

---
+++ Assets/SwipeAndTapManager.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeAndTapManager : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float tapDetectionThreshold = 0.2f;
+    [SerializeField] private LayerMask enemyLayerMask;
+    [SerializeField] private TrailRenderer swipeTrail;
+
+    [Header("References")]
+    [SerializeField] private game_maneger gameManager;
+
+    float swipeAngle = 0;
+
+    private Vector2 touchStartPosition;
+    private bool isTouching = false;
+    private float touchStartTime;
+
+
+    public AudioSource Miss_the_cut_Sound;
+
+    public ParticleSystem missEffect;
+
+    public AudioSource Kill_the_Enemy_Sound;
+    private void Awake()
+    {
+        Time.timeScale = 1.0f;
+
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount == 0) return;
+
+        Touch t = Input.GetTouch(0);
+
+        if (t.phase == TouchPhase.Began)
+            HandleTouchBegan(t.position);
+        else if (t.phase == TouchPhase.Moved)
+            HandleTouchMoved(t.position);
+        else if (t.phase == TouchPhase.Ended || t.phase == TouchPhase.Canceled)
+            HandleTouchEnded(t.position);
+    }
+
+    private void HandleTouchBegan(Vector2 screenPosition)
+    {
+        Vector3 wp = Camera.main.ScreenToWorldPoint(screenPosition);
+        wp.z = 0;
+
+        touchStartPosition = new Vector2(wp.x, wp.y);
+
+        touchStartTime = Time.time;
+        isTouching = true;
+
+        if (swipeTrail != null)
+        {
+            swipeTrail.enabled = false;        // خاموش کامل
+            swipeTrail.Clear();                // پاک کردن کش
+            swipeTrail.transform.position = wp; // تلپورت به جای انگشت
+            swipeTrail.enabled = true;         // روشن
+            swipeTrail.emitting = true;        // شروع رسم
+        }
+
+    }
+
+    private void HandleTouchMoved(Vector2 screenPosition)
+    {
+        if (!isTouching || swipeTrail == null) return;
+
+        Vector3 wp = Camera.main.ScreenToWorldPoint(screenPosition);
+        wp.z = 0;
+        swipeTrail.transform.position = wp;
+
+    }
+
+
+    void PlayMissEffect(Vector3 pos)
+    {
+        if (missEffect == null) return;
+
+        missEffect.transform.position = pos;
+        missEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        missEffect.Play();
+    }
+
+    private void HandleTouchEnded(Vector2 screenPosition)
+    {
+        if (!isTouching) return;
+        isTouching = false;
+
+
+        Vector3 wp = Camera.main.ScreenToWorldPoint(screenPosition);
+        Vector2 endPosition = new Vector2(wp.x, wp.y);
+
+        Vector2 delta = endPosition - touchStartPosition;
+        float dist = delta.magnitude;
+
+
+        if (swipeTrail != null)
+        {
+            swipeTrail.emitting = false;  // دیگه نقطه جدید نگیره
+            swipeTrail.enabled = false;   // کلاً خاموشش کن
+        }
+
+
+        // تشخیص Tap
+        if (dist <= tapDetectionThreshold)
+        {
+            PerformTapAction(endPosition);
+            return;
+        }
+
+        // تشخیص Swipe
+        if (dist < 0.1f) return;
+
+        delta.Normalize();
+        swipeAngle = Vector2.SignedAngle(Vector2.right, delta);
+
+        RaycastHit2D hit = Physics2D.Linecast(touchStartPosition, endPosition, enemyLayerMask);
+        if (!hit) return;
+
+        EnemyScript es = hit.collider.GetComponent<EnemyScript>();
+        if (es == null) return;
+
+        ProcessEnemyHit(hit.collider.gameObject, es, swipeAngle);
+    }
+    private void PerformTapAction(Vector2 tapPosition)
+    {
+        Collider2D col = Physics2D.OverlapPoint(tapPosition, enemyLayerMask);
+        if (col == null) return;
+
+        EnemyScript enemyScript = col.GetComponent<EnemyScript>();
+        if (enemyScript == null) return;
+
+        GameObject enemy = col.gameObject;
+
+        bool sliced = enemyScript.IsInSlice(swipeAngle);
+
+        if (enemyScript.enemyType == EnemyType.Friend)
+        {
+            gameManager.AddScore(15);
+            KillEnemy(enemy);
+            return;
+        }
+
+      
+    }
+
+    private void ProcessEnemyHit(GameObject enemy, EnemyScript enemyScript, float swipeAngle)
+    {
+        bool sliced = enemyScript.IsInSlice(swipeAngle);
+
+        if (enemyScript.enemyType == EnemyType.Friend)
+        {
+            gameManager.LostOneLife();
+            KillEnemy(enemy);
+            return;
+        }
+
+        if (enemyScript.enemyType == EnemyType.Enemy )
+        {
+            if (sliced)
+            {
+                gameManager.AddScore(15);
+                KillEnemy(enemy);
+            }
+            else
+            {
+                
+                Miss_the_cut_Sound.Play();
+                PlayMissEffect(enemy.transform.position);
+            }
+            return;
+        }
+
+        if (enemyScript.enemyType == EnemyType.Reproducible)
+        {
+            if (sliced)
+            {
+                gameManager.AddScore(25);
+              
+                gameManager.Add_Gost_Kids(enemy.transform);
+                
+                KillEnemy(enemy);
+            }
+            else
+            {
+                enemyScript.moveSpeed += 0.5f;
+                Miss_the_cut_Sound.Play();
+                PlayMissEffect(enemy.transform.position);
+            }
+            return;
+        }
+
+        if (enemyScript.enemyType == EnemyType.Boss)
+        {
+            Debug.Log("Boss sliced? (Not implemented)");
+        }
+    }
+
+    private void KillEnemy(GameObject enemy)
+    {
+        if (enemy != null)
+            Kill_the_Enemy_Sound.Play();
+            Destroy(enemy);
+    }
+}
---
+++ Assets/game_maneger.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+
+public class game_maneger : MonoBehaviour
+{
+    public int Score = 0;
+
+    public int life = 4;
+
+    public TextMeshProUGUI Score_Text;
+
+
+    public GameObject Enemy_Prifabs;
+
+
+    public GameObject Enemy_Prifabs1;
+
+
+
+    public GameObject Win_Panel;
+
+    public GameObject Lost_panel;
+
+    [Header("Lifs")]
+    public GameObject Life1;
+    public GameObject Life2;
+    public GameObject Life3;
+    public GameObject Life4;
+
+
+    public void LostOneLife()
+    {
+        life -= 1;
+
+        // حذف یکی از جانها UI
+        if (life == 3)
+            Life4.SetActive(false);
+        else if (life == 2)
+            Life3.SetActive(false);
+        else if (life == 1)
+            Life2.SetActive(false);
+        else if (life == 0)
+            Life1.SetActive(false);
+
+      
+
+        if (life == 0)
+        {
+            You_lost();
+        }
+    }
+
+
+    public void AddScore( int Ammont)
+    {
+        Score += Ammont;
+        Score_Text.text = "Score : " + Score;
+
+        if (Score >= 150  ) {
+        You_win();
+        }
+    }
+
+    public void You_lost()
+    {
+
+        Lost_panel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void reset_Game()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void You_win()
+    {
+
+        Win_Panel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Add_Gost_Kids(Transform gost_transform)
+    {
+
+        Debug.Log("test");
+        gost_transform.position = new Vector3(gost_transform.position.x-6
+            , gost_transform.position.y, gost_transform.position.z) ;
+
+      
+        Instantiate(Enemy_Prifabs , gost_transform ) ;
+
+        GameObject enemy = Instantiate(Enemy_Prifabs, gost_transform.position, Quaternion.identity);
+        enemy.GetComponent<EnemyScript>().gameManager = this;
+
+
+
+        gost_transform.position = new Vector3(gost_transform.position.x-1, gost_transform.position.y+0.5f , gost_transform.position.z);
+
+
+        GameObject enemy1 = Instantiate(Enemy_Prifabs1, gost_transform.position, Quaternion.identity);
+        enemy1.GetComponent<EnemyScript>().gameManager = this;
+    }
+
+
+
+}
